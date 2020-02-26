6e0db75e0b6d87a4e91f40ba9573907b0a77e4fb
Ely Gandara <ely.gandara.c@gmail.com>
2020-02-26 08:11:54 -0700

    Added gitignore.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gun_master_9000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
